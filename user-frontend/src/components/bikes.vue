f7cc3ecb244241b84406374ffbec68041ec66bc5
saikittu332 <saikittu332@github.com>
2019-10-04 02:18:41 +0530

    modiefied filter

--- user-frontend/src/components/bikes.vue
+++ user-frontend/src/components/bikes.vue
@@ -1,12 +1,48 @@
 <template>
 <div class="bikes">
 <div id="myFilter" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" v-on:click="closeFilter">&times;</a>
-  <div class="mt-3">
-      <button class="accordion">Section 1</button>
-    <div class="panel" v-show="tr">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <i class="fil fa fa-filter" aria-hidden="true"></i>
+    <span class="fil sc-bYSBpT iQnyqm">Filters</span>
+    <i class="close fa fa-times" v-on:click="closeFilter" aria-hidden="true"></i>
+
+  <!-- <a href="javascript:void(0)" class="closebtn" v-on:click="closeFilter">&times;</a> -->
+    <!-- <button type="button" class="close" aria-label="Close" v-on:click="closeFilter">
+        <span aria-hidden="true">&times;</span>
+    </button> -->
+  <div class="manufac">
+      <button class="manufacturer" v-on:click="toggle = !toggle">Manufacturer
+          <a v-if="!toggle"><i class="fas fa-caret-down fa-sm"></i></a>
+          <a v-if="toggle"><i class="fas fa-caret-up fa-sm"></i></a>
+        </button>
+        <div class="panel" v-if="toggle">
+        <a href="suzuki">Suzuki</a>
+        <a href="Maruti">Maruti</a>
+        <a href="Honda">Honda</a>
+        <a href="Tvs">Tvs</a>
+        </div>
     </div>
+    <div class="bikecc">
+        <button class="cc" v-on:click="toggle = !toggle">CC
+          <a v-if="!toggle"><i class="fas fa-caret-down"></i></a>
+          <a v-if="toggle"><i class="fas fa-caret-up"></i></a>
+        </button>
+        <div class="panel" v-if="toggle">
+        <a href="100cc">From 100cc</a>
+        <a href="200cc">101cc-200cc</a>
+        <a href="300cc">201cc-300cc</a>
+        <a href="400cc">301cc-400cc</a>
+        </div>
+  </div>
+  <div class="variants">
+        <button class="type" v-on:click="toggle = !toggle">Type
+          <a v-if="!toggle"><i class="fas fa-caret-down"></i></a>
+          <a v-if="toggle"><i class="fas fa-caret-up"></i></a>
+        </button>
+        <div class="panel" v-if="toggle">
+        <a href="petrol">Petrol</a>
+        <a href="diesel">Diesel</a>
+        <a href="electric">Electric</a>
+        </div>
   </div>
 </div>
 
@@ -87,7 +123,7 @@ export default {
     // },
     data(){
         return {
-            tr:true
+            toggle:false
         }
     },
     methods:{
@@ -122,9 +158,11 @@ body {
      outline: none; 
 }
 .card-img-top{
-    height: 175px;
-    
-   
+    height: 175px; 
+    width: auto;
+}
+.dropdown-menu.show{
+    left: -25px !important;
 }
 .row{
      margin-top: 5px ;
@@ -133,21 +171,9 @@ body {
     background-color: whitesmoke;
     color:black;
     border:none;
-    font-size: 16px
+    font-size: 16px;
 }
-.dropdown button:hover{
-    background-color: none;
-    outline: none
-}
-.btn-secondary:not(:disabled):not(.disabled).active, .btn-secondary:not(:disabled):not(.disabled):active, .show>.btn-secondary.dropdown-toggle {
-    color: black;
-    background-color: none !important;
-    outline: none;
-    border: none
-}
-.mobile{
-    display: none;
-}
+
 .notify-badge{
     position: absolute;
     right:4px;
@@ -170,12 +196,12 @@ body {
   width: 0;
   position: fixed;
   z-index: 1;
-  top: 76px;
+  /* top: 76px; */
   left: 0;
   background-color: white;
   overflow-x: hidden;
   transition: 0.5s;
-  padding-top: 60px;
+  padding-top: 35px;
 }
 
 .sidenav a {
@@ -186,17 +212,17 @@ body {
   display: block;
   transition: 0.3s;
 }
-
-.sidenav a:hover {
-  color: red;
+.close{
+    margin-top: 3px;
+    margin-right: 15px;
+    size: 50px;
 }
 
-.sidenav .closebtn {
-  position: absolute;
-  top: 0px;
- right: 2px;
-  font-size: 36px;
-  /* margin-left: 50px; */
+.fil {
+    margin-left: 2px;
+    
+    font-size:20px;
+    size: 50px;
 }
 
 #main {
@@ -210,30 +236,78 @@ body {
   .sidenav {padding-top: 15px;}
   .sidenav a {font-size: 18px;}
 }
-.accordion {
-  background-color: #eee;
-  color: #444;
+.manufacturer  {
+  background-color: rgb(190, 190, 190);
+  /* color: #444; */
   cursor: pointer;
-  padding: 18px;
+  /* padding: 10px 10px 10px 10px; */
   width: 100%;
   border: none;
-  text-align: left;
+  /* text-align: left; */
   outline: none;
   font-size: 15px;
   transition: 0.4s;
+  margin-top: 15px;
 }
 
-.accordion:hover {
-  background-color: #ccc;
+.cc  {
+  background-color: rgb(190, 190, 190);
+  /* color: #444; */
+  cursor: pointer;
+  /* padding: 10px 10px 10px 10px; */
+  width: 100%;
+  border: none;
+  /* text-align: left; */
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+  margin-top: 15px;
+}
+
+.type {
+  background-color: rgb(190, 190, 190);
+  /* color: #444; */
+  cursor: pointer;
+  /* padding: 10px 10px 10px 10px; */
+  width: 100%;
+  border: none;
+  /* text-align: left; */
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+  margin-top: 15px;
+}
+
+.cc a{
+  /* color: #777; */
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
+}
+
+.type a{
+  /* color: #777; */
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
 }
 
 
+.manufacturer  a{
+  /* color: #777; */
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
+}
 
 .panel {
   padding: 0 18px;
   background-color: white;
-  max-height: 0;
   overflow: hidden;
   transition: max-height 0.2s ease-out;
 }
+.panel a{
+      font-size: 15px;
+}
+
 </style>
--- user-frontend/src/components/navigation.vue
+++ user-frontend/src/components/navigation.vue
@@ -7,13 +7,13 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
        <li class="nav-item">
         <a class="nav-link"><router-link to="/about"  exact-active-class="active">About</router-link></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link"><router-link to="/bikes"  exact-active-class="active">Bikes</router-link></a>
+        <a class="nav-link" href="/bikes">Bikes</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">How It Works ?</a>
